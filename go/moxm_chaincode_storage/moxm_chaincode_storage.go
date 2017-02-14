ab743a215db42fb3fe6f95f4b442c9d361e2f6ec
richard.ma@msn.com <richard.ma@msn.com>
2017-02-14 12:05:52 +0800

    go

--- go/moxm_chaincode_storage/moxm_chaincode_storage.go
+++ go/moxm_chaincode_storage/moxm_chaincode_storage.go
@@ -42,15 +42,27 @@ func (t *StorageChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 func (t *StorageChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	fmt.Println("ex02 Invoke")
 	function, args := stub.GetFunctionAndParameters()
-	if function == "put" {
-		// 存
-		return t.put(stub, args)
-	} else if function == "delete" {
+
+	if function != "invoke" {
+		return shim.Error("Unknown function call")
+	}
+
+	if len(args) < 2 {
+		return shim.Error("Incorrect number of arguments. Expecting at least 2")
+	}
+
+	if args[0] == "delete" {
 		// Deletes an entity from its state
 		return t.delete(stub, args)
-	} else if function == "query" {
-		// the old "Query" is now implemtned in invoke
+	}
+
+	if args[0] == "query" {
+		// queries an entity state
 		return t.query(stub, args)
+	}
+	if args[0] == "put" {
+		// Deletes an entity from its state
+		return t.put(stub, args)
 	}
 
 	return shim.Error("Invalid invoke function name. Expecting \"put\" \"delete\" \"query\"")
@@ -62,12 +74,12 @@ func (t *StorageChaincode) put(stub shim.ChaincodeStubInterface, args []string) 
 	var Aval string // Asset holdings
 	var err error
 
-	if len(args) != 2 {
+	if len(args) != 3 {
 		return shim.Error("Incorrect number of arguments. Expecting 3")
 	}
 
-	A = args[0]
-	Aval = args[1]
+	A = args[1]
+	Aval = args[2]
 
 	// Write the state back to the ledger
 	err = stub.PutState(A, []byte(Aval))
@@ -80,11 +92,11 @@ func (t *StorageChaincode) put(stub shim.ChaincodeStubInterface, args []string) 
 
 // Deletes an entity from state
 func (t *StorageChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 1 {
+	if len(args) != 2 {
 		return shim.Error("Incorrect number of arguments. Expecting 1")
 	}
 
-	A := args[0]
+	A := args[1]
 
 	// Delete the key from the state in ledger
 	err := stub.DelState(A)
@@ -100,11 +112,11 @@ func (t *StorageChaincode) query(stub shim.ChaincodeStubInterface, args []string
 	var A string // Entities
 	var err error
 
-	if len(args) != 1 {
+	if len(args) != 2 {
 		return shim.Error("Incorrect number of arguments. Expecting name of the person to query")
 	}
 
-	A = args[0]
+	A = args[1]
 
 	// Get the state from the ledger
 	Avalbytes, err := stub.GetState(A)
